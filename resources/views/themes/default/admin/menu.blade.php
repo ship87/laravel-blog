dd7123b2cec271f172c793c6f59fe12864e4c342
Yusuf <15554011@firat.edu.tr>
2019-03-22 09:19:26 +0200

    fixed admin menu url and seed factories

--- resources/views/themes/default/admin/menu.blade.php
+++ resources/views/themes/default/admin/menu.blade.php
@@ -1,36 +1,36 @@
-<p><a href="{{ config('app.url_admin').'/posts' }}">{{ u__('admin.posts') }}</a></p>
+<p><a href="{{ url(config('app.url_admin').'/posts') }}">{{ u__('admin.posts') }}</a></p>
 
-<p><a href="{{ config('app.url_admin').'/posts/create' }}">{{ u__('admin.create post') }}</a></p>
+<p><a href="{{ url(config('app.url_admin').'/posts/create') }}">{{ u__('admin.create post') }}</a></p>
 <hr>
-<p><a href="{{ config('app.url_admin').'/post-comments' }}">{{ u__('admin.post comments') }}</a></p>
+<p><a href="{{ url(config('app.url_admin').'/post-comments') }}">{{ u__('admin.post comments') }}</a></p>
 <hr>
-<p><a href="{{ config('app.url_admin').'/categories' }}">{{ u__('admin.categories') }}</a></p>
+<p><a href="{{ url(config('app.url_admin').'/categories') }}">{{ u__('admin.categories') }}</a></p>
 
-<p><a href="{{ config('app.url_admin').'/categories/create' }}">{{ u__('admin.create category') }}</a></p>
+<p><a href="{{ url(config('app.url_admin').'/categories/create') }}">{{ u__('admin.create category') }}</a></p>
 <hr>
-<p><a href="{{ config('app.url_admin').'/tags' }}">{{ u__('admin.tags') }}</a></p>
+<p><a href="{{ url(config('app.url_admin').'/tags') }}">{{ u__('admin.tags') }}</a></p>
 
-<p><a href="{{ config('app.url_admin').'/tags/create' }}">{{ u__('admin.create tag') }}</a></p>
+<p><a href="{{ url(config('app.url_admin').'/tags/create') }}">{{ u__('admin.create tag') }}</a></p>
 <hr>
-<p><a href="{{ config('app.url_admin').'/pages' }}">{{ u__('admin.pages') }}</a></p>
+<p><a href="{{ url(config('app.url_admin').'/pages') }}">{{ u__('admin.pages') }}</a></p>
 
-<p><a href="{{ config('app.url_admin').'/pages/create' }}">{{ u__('admin.create page') }}</a></p>
+<p><a href="{{ url(config('app.url_admin').'/pages/create') }}">{{ u__('admin.create page') }}</a></p>
 <hr>
-<p><a href="{{ config('app.url_admin').'/page-comments' }}">{{ u__('admin.page comments') }}</a></p>
-<hr>
-
-<p><a href="{{ config('app.url_admin').'/images' }}">{{ u__('admin.images') }}</a></p>
-<hr>
-<p><a href="{{ config('app.url_admin').'/users' }}">{{ u__('admin.users') }}</a></p>
-
-<p><a href="{{ config('app.url_admin').'/users/create' }}">{{ u__('admin.create user') }}</a></p>
+<p><a href="{{ url(config('app.url_admin').'/page-comments') }}">{{ u__('admin.page comments') }}</a></p>
 <hr>
 
-<p><a href="{{ config('app.url_admin').'/permissions' }}">{{ u__('admin.permissions') }}</a></p>
-
-<p><a href="{{ config('app.url_admin').'/permissions/create' }}">{{ u__('admin.create permission') }}</a></p>
+<p><a href="{{ url(config('app.url_admin').'/images') }}">{{ u__('admin.images') }}</a></p>
 <hr>
-<p><a href="{{ config('app.url_admin').'/roles' }}">{{ u__('admin.roles') }}</a></p>
+<p><a href="{{ url(config('app.url_admin').'/users') }}">{{ u__('admin.users') }}</a></p>
 
-<p><a href="{{ config('app.url_admin').'/roles/create' }}">{{ u__('admin.create role') }}</a></p>
+<p><a href="{{ url(config('app.url_admin').'/users/create') }}">{{ u__('admin.create user') }}</a></p>
+<hr>
+
+<p><a href="{{ url(config('app.url_admin').'/permissions') }}">{{ u__('admin.permissions') }}</a></p>
+
+<p><a href="{{ url(config('app.url_admin').'/permissions/create') }}">{{ u__('admin.create permission') }}</a></p>
+<hr>
+<p><a href="{{ url(config('app.url_admin').'/roles') }}">{{ u__('admin.roles') }}</a></p>
+
+<p><a href="{{ url(config('app.url_admin').'/roles/create') }}">{{ u__('admin.create role') }}</a></p>
 <hr>
